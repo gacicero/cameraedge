571eef2ad300f7ff34f4eb178f408d03797f1b7e
lilysnow14 <146676277+lilysnow14@users.noreply.github.com>
2023-12-03 15:03:57 -0500

    DEbug

---
+++ openCV/build/intermediates/cxx/Debug/4j4r2f5m/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\16033\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\16033\\OneDrive\\Documents\\GitHub\\cameraedge\\openCV\\.cxx\\Debug\\4j4r2f5m\\x86_64" ^
+  opencv_jni_shared
