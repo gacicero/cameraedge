36d6a21e28106c0fe517c16721642f88830632b2
lilysnow14 <146676277+lilysnow14@users.noreply.github.com>
2023-12-03 17:13:48 -0500

    ColorMap

--- app/src/main/java/com/example/cameraedge/MainActivity.java
+++ app/src/main/java/com/example/cameraedge/MainActivity.java
@@ -28,9 +28,38 @@ import java.util.List;
 
 public class MainActivity extends CameraActivity {
 
+
+
     CameraBridgeViewBase cameraBridgeViewBase;
     private Button startButton;
+    private Button greenButton;
+
+
+    private Button blueButton;
+
+
+    private Button redButton;
+
+    private Button pinkButton;
+
+
+    private Button yellowButton;
+
+
+    private Button originalButton;
+
     private boolean isStartButtonClicked = false;
+
+    private boolean isGreen = false;
+    private boolean isRed = false;
+    private boolean isBlue = false;
+
+    private boolean isPink = false;
+    private boolean isYellow = false;
+    private boolean isOriginal = false;
+
+
+
 
     private TextView welcomeTextView;
     private TextView startTextView;
@@ -43,6 +72,13 @@ public class MainActivity extends CameraActivity {
         welcomeTextView = findViewById(R.id.welcomeTextView);
         startTextView = findViewById(R.id.startTextView);
         startButton = findViewById(R.id.startButton);
+        greenButton = findViewById(R.id.greenButton);
+        redButton = findViewById(R.id.redButton);
+        blueButton = findViewById(R.id.blueButton);
+        pinkButton = findViewById(R.id.pinkButton);
+        yellowButton = findViewById(R.id.yellowButton);
+        originalButton = findViewById(R.id.originalButton);
+
         startButton.setOnClickListener(new View.OnClickListener() {
                                            @Override
                                            public void onClick(View v) {
@@ -58,7 +94,113 @@ public class MainActivity extends CameraActivity {
 
     });
 
-            cameraBridgeViewBase = findViewById(R.id.cameraView);
+        greenButton.setOnClickListener(new View.OnClickListener() {
+                                           @Override
+                                           public void onClick(View v) {
+                                               //vanish startButton
+                                               isGreen = true;
+                                               isBlue = false;
+                                               isRed = false;
+                                               isYellow = false;
+                                               isOriginal = false;
+                                               isPink = false;
+
+
+                                           }
+                                       });
+
+
+
+        blueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isGreen = false;
+                isRed = false;
+                isBlue = true;
+                isYellow = false;
+                isPink = false;
+                isOriginal = false;
+
+
+
+
+
+            }});
+
+        redButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    isGreen = false;
+                    isRed = true;
+                    isBlue = false;
+                    isYellow = false;
+                    isPink = false;
+                    isOriginal = false;
+
+
+
+                }});
+
+
+        yellowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isGreen = false;
+                isRed = false;
+                isBlue = false;
+                isYellow = true;
+                isPink = false;
+                isOriginal = false;
+
+
+
+
+
+            }});
+        pinkButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    isGreen = false;
+                    isRed = false;
+                    isBlue = false;
+                    isYellow = false;
+                    isPink = true;
+                    isOriginal = false;
+
+
+
+
+
+                }});
+
+
+                originalButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        isGreen = false;
+                        isRed = false;
+                        isBlue = false;
+                        isYellow = false;
+                        isPink = false;
+                        isOriginal = true;
+
+
+
+
+
+                    }});
+
+
+
+
+
+
+
+
+
+
+
+                    cameraBridgeViewBase = findViewById(R.id.cameraView);
         cameraBridgeViewBase.setCvCameraViewListener(new CameraBridgeViewBase.CvCameraViewListener2() {
             @Override
             public void onCameraViewStarted(int width, int height) {
@@ -78,7 +220,48 @@ public class MainActivity extends CameraActivity {
                 Imgproc.warpAffine(gray, gray, rotationMatrix, gray.size());
                 Imgproc.Canny(gray, gray, 80, 100);
 
+                if (isGreen == true)
+                {
+                    Mat color = new Mat();
+                    Imgproc.applyColorMap(gray,color,Imgproc.COLORMAP_SUMMER);
+                    return color;
+                }
+                if (isRed == true)
+                {
+                    Mat color = new Mat();
+                    Imgproc.applyColorMap(gray,color,Imgproc.COLORMAP_JET);
+                    return color;
+                }
+                if (isBlue == true)
+                {
+                    Mat color = new Mat();
+                    Imgproc.applyColorMap(gray,color,Imgproc.COLORMAP_AUTUMN);
+                    return color;
+                }
+
+                if(isYellow == true)
+                {
+                    Mat color = new Mat();
+                    Imgproc.applyColorMap(gray,color,Imgproc.COLORMAP_COOL);
+                    return color;
+                }
+                if(isPink == true)
+                {
+                    Mat color = new Mat();
+                    Imgproc.applyColorMap(gray,color,Imgproc.COLORMAP_SPRING);
+                    return color;
+                }
+                if(isOriginal == true)
+                {
+                    Mat color = new Mat();
+                    Imgproc.applyColorMap(gray,color,Imgproc.COLORMAP_BONE);
+                    return color;
+                }
+
                 return gray;
+
+
+
 
             }
         });
