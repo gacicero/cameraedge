9c8ed0d3eb5ffaa19fb1f1aaf4b71ffc249c27e6
lilysnow14 <146676277+lilysnow14@users.noreply.github.com>
2023-12-04 13:55:36 -0500

    Format Fix

--- openCV/build/intermediates/cxx/Debug/4j4r2f5m/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\jackh\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\jackh\\Documents\\GitHub\\cameraedge\\openCV\\.cxx\\Debug\\4j4r2f5m\\armeabi-v7a" ^
-  opencv_jni_shared
--- openCV/build/intermediates/cxx/Debug/4j4r2f5m/logs/x86/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\jackh\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\jackh\\Documents\\GitHub\\cameraedge\\openCV\\.cxx\\Debug\\4j4r2f5m\\x86" ^
-  opencv_jni_shared
